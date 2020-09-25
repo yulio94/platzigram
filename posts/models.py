79070bd152c5eac3b79ca1cd70dcc7c22f65b011
Julio Estrada <32644849+yulio94@users.noreply.github.com>
2020-09-24 21:59:41 -0600

    Feature/posts (#4)
    * posts app stared
    
    * Initial configuration of posts app
    
    * feat: migrations was added.
    
    * feat: migrations was added.
    
    * feat: templates was added and migrations was deleted
    
    * feat: migrations, media and static directories was added
    
    * feat: post model was added.
    
    * feat: static's url and root was added.
    
    * feat: More urls was added.

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+"""Post models."""
 
-# Create your models here.
+# Django
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    """Post model."""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='posts/photos')
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        """Return title and username
+
+        :return str:
+        """
+
+        return f'{self.title} by @{self.user.username}'
+      
